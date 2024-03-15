469ce3c578a96680d5e7b4f3998ef9d50e226613
Chan De Wei <deweichan@gmail.com>
2024-03-16 01:02:55 +0800

    try again

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,7 +1,7 @@
 const apiUrl = () => {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.REACT_APP_NODE_ENV === 'production') {
         return 'http://webthreewallet.netlify.app/.netlify/functions/api';
-    } else if (process.env.NODE_ENV === 'development') {
+    } else if (process.env.REACT_APP_NODE_ENV === 'development') {
         return 'http://localhost:3001';
     } else {
         return 'http://localhost:3001'; // Default URL for other environments

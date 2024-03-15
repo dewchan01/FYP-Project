5df992402c21c9cd52e29f331520bde9d8693992
Chan De Wei <deweichan@gmail.com>
2024-03-16 03:01:16 +0800

    try again

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,6 +1,6 @@
 const apiUrl = () => {
     if (process.env.REACT_APP_NODE_ENV === 'production') {
-        return 'http://webthreewalletapi.netlify.app/.netlify/functions/api';
+        return 'https://webthreewalletapi.netlify.app/.netlify/functions/api';
     } else if (process.env.REACT_APP_NODE_ENV === 'development') {
         return 'http://localhost:3001';
     } else {

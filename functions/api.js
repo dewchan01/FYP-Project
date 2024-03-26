51edbde0265fc6ead95c69decf567cf4575bbe96
Chan De Wei <deweichan@gmail.com>
2024-03-26 09:26:55 +0800

    try again

--- functions/api.js
+++ functions/api.js
@@ -10,9 +10,9 @@ const web3 = createAlchemyWeb3(process.env.POLYGON_RPC_URL);
 const db = require('../db/conn');
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "GET,PATCH,OPTIONS,POST,PUT,DELETE");
+  // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content,Content-Type, Accept, Authorization");
   next();
 });
 

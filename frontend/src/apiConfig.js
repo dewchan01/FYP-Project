cfd97d2f2ed32a7f41048e5388ce633a7b76b968
Chan De Wei <deweichan@gmail.com>
2024-03-28 18:06:44 +0800

    update apiconfig

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,6 +1,6 @@
 const apiUrl = () => {
     if (process.env.REACT_APP_NODE_ENV === 'production') {
-        return 'https://webthreewalletapi.netlify.app/.netlify/functions/api';
+        return 'https://web3wallet-migrate-api.netlify.app/.netlify/functions/api';
     } else if (process.env.REACT_APP_NODE_ENV === 'development') {
         return 'http://localhost:3001';
     } else {

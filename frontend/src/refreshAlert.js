cd3d192c8901f3a5ef49ca0fb7ab88165ce4e632
Chan De Wei <deweichan@gmail.com>
2024-04-13 10:15:07 +0800

    try again for refresh alert

--- frontend/src/refreshAlert.js
+++ frontend/src/refreshAlert.js
@@ -1,7 +1,12 @@
 function refreshAlert({ address }) {
     if (!address) {
         alert("Page Refreshed Detected! Please Connect Your Account Again!");
-        window.location.href = "/";
+        if (process.env.REACT_APP_NODE_ENV === 'production'){
+            window.location.href = "https://web3wallet-migrate.netlify.app";
+        }
+        else{
+            window.location.href = "http://localhost:3001";
+        }
         return;
     }
 }
